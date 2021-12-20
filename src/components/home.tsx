cb5fe90c50c792f6670c9b8792809916c033a543
zaidschouwey98 <zazar981@hotmail.com>
2021-12-20 16:06:31 +0100

    First commit with template

---
+++ src/components/home.tsx
@@ -0,0 +1,19 @@
+
+function Home() {
+    return (
+        <div className="parallax-content baner-content" id="home">
+            <div className="container">
+                <div className="text-content">
+                    <h2><b>Front-end Developer</b></h2>
+                    <p>Tinker is a responsive CSS template provided by templatemo site. This design can be applied for any
+                        kind of website. Please mention our site to your colleagues. Thank you.</p>
+                    <div className="primary-white-button">
+                        <a href="#" className="scroll-link" data-id="about">Let's Start</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+export default Home;
---
+++ src/components/skills.tsx
@@ -0,0 +1,92 @@
+import anime from 'animejs'
+const ScrollMagic = require('scrollmagic');
+
+
+
+function Skills() {
+    const controller = new ScrollMagic.Controller();
+   
+    console.log("tst")
+    
+    new ScrollMagic.Scene({
+        triggerElement: "#about",
+        triggerHook: 0.8, // show, when scrolled 10% into view
+        duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+        offset: 50 // move trigger to center of element
+      })
+        .on("progress", (scroll:object) => {
+            var animateProgressPhp = anime({
+                targets: '#progress-php',
+                width: '80%',
+                easing: 'linear',
+            });
+            var animateProgressJs = anime({
+                targets: '#progress-js',
+                width: '75%',
+                easing: 'linear',
+            });
+            var animateProgressRuby = anime({
+                targets: '#progress-ruby',
+                width: '50%',
+                easing: 'linear',
+            });
+            var animateProgressUnity = anime({
+                targets: '#progress-unity',
+                width: '72%',
+                easing: 'linear',
+            });
+            var animateProgressReact = anime({
+                targets: '#progress-react',
+                width: '83%',
+                easing: 'linear',
+            });
+        })
+        .addIndicators()
+        .addTo(controller);
+    return (
+        <section id="about" className="page-section">
+        <div className="container">
+         
+                <div className="text-content text-center">
+                    <h2><b>My skills</b></h2>
+                </div>
+         
+            <canvas className="secondbackground"></canvas>
+            <div className="row">
+                <div className="col-xs-2 text-center"><img src="img/php.png" className="language-icons"/></div>
+                <div className="col-xs-10 progress">
+                <div id="progress-php" className="progress-bar" role="progressbar" style={{width:0}} aria-valuenow={25}
+                        aria-valuemin={0} aria-valuemax={100}></div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-xs-2 text-center"><img src="./img/js.png" className="language-icons"/></div>
+                <div className="col-xs-10 progress">
+                    <div id="progress-js" className="progress-bar" role="progressbar" style={{width: 0}} ></div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-xs-2 text-center"><img src="./img/ruby.png" className="language-icons"/></div>
+                <div className="col-xs-10 progress">
+                    <div id="progress-ruby" className="progress-bar" role="progressbar" style={{width: 0}} ></div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-xs-2 text-center"><img src="./img/unity.png" className="language-icons"/></div>
+                <div className="col-xs-10 progress">
+                    <div id="progress-unity" className="progress-bar" role="progressbar" style={{width: 0}} ></div>
+                </div>
+            </div>
+            
+            <div className="row">
+                <div className="col-xs-2 text-center"><img src="./img/react.png" className="language-icons"/></div>
+                <div className="col-xs-10 progress">
+                    <div id="progress-react" className="progress-bar" role="progressbar" style={{width: 0}}></div>
+                </div>
+            </div>
+            
+        </div>
+    </section>
+    )
+}
+export default Skills;
